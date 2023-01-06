c5d4b13985406377e67dbc44e6e37f60268c839e
“Nyamekesse” <snyamekesse@stu.ucc.edu.gh>
2023-01-06 13:19:12 +0000

    updated source

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+# 3-say_my_name.py
+"""Defines a name-printing function."""
+
+
 def say_my_name(first_name, last_name=""):
-    '''FUNCTION TO PRINT FIRST AND LAST NAME
-       Args:
-       para1 = string
-       para2 = string
-       Raise:
-       TypeError if first or last name not a string
-       Return:
-       None
-    '''
-    if type(first_name) != str:
+    """Print a name.
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print(f"My name is {first_name} {last_name}", end="")
+    print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+# 4-print_square.py
+"""Defines a square-printing function."""
+
+
 def print_square(size):
-    '''FUNCTION TO PRINT # IN LENGHT OF SIZE
-       Args:
-           para1 = interger
-           para2 = interger
-        Raise:
-            TypError if size is not an integer
-            ValueError if size is less than zero
-        Return:
-            None
-    '''
-    if type(size) != int:
+    """Print a square with the # character.
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
-    if type(size) == float and size < 0:
-        raise TypeError("size must be an integer")
+        raise ValueError("size must be >= 0")
+
     for i in range(size):
-        print('#' * size)
+        [print("#", end="") for j in range(size)]
+        print("")
